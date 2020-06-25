e3e0a831d4831b2038be61003b456930c7c22051
Aseem Shukla <aseem.a.shukla@gmail.com>
2020-06-25 01:41:17 -0400

    new test.py

--- test.py
+++ test.py
@@ -1,14 +1,18 @@
-import pandas as pd
-import datetime as dt
+# import pandas as pd
+# import datetime as dt
 
-yesterday = dt.date.today() - dt.timedelta(days = 1)
-yesterday = yesterday.strftime("%m-%d-%Y")
-url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/" 
-url_date = url + str(yesterday) + ".csv"
+# yesterday = dt.date.today() - dt.timedelta(days = 1)
+# yesterday = yesterday.strftime("%m-%d-%Y")
+# url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/" 
+# url_date = url + str(yesterday) + ".csv"
 
-print(url_date)
+# print(url_date)
 
-df_daily = pd.read_csv(url_date)
-df_daily['Date'] = yesterday
+# df_daily = pd.read_csv(url_date)
+# df_daily['Date'] = yesterday
 
-df_daily.to_csv('test_df.csv')
+# df_daily.to_csv('test_df.csv')
+
+import numpy as np
+
+print(np.sum([5,10]))
